b5ab286657368b37c3d6ac53b3ddf9115b37646b
tianle.li <tianle.li@qunar.com>
2019-04-24 19:28:35 +0800

    update demo

---
+++ src/main/code/com/abecedarian/demo/spark/SparkHelloWord.scala
@@ -0,0 +1,26 @@
+package com.abecedarian.demo.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by abecedarian on 2019/3/28
+  *
+  */
+object SparkHelloWord extends App {
+
+  override def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+    conf.setAppName("SparkHelloWorld")
+    conf.setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.parallelize(Seq("hello world", "hello demo-tutorial"))
+    val wc = lines.flatMap(_.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
+    wc.foreach(println)
+
+
+    Thread.sleep(10 * 60 * 1000) // 挂住 10 分钟; 这时可以去看 SparkUI: http://localhost:4040
+  }
+
+}
