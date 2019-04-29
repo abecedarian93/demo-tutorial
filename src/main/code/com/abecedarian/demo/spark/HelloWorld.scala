d9afe81463f612ca02fea5e326f132ac1d06b578
tianle.li <tianle.li@qunar.com>
2019-04-29 20:56:30 +0800

    update scala

--- src/main/code/com/abecedarian/demo/spark/HelloWorld.scala
+++ src/main/code/com/abecedarian/demo/spark/HelloWorld.scala
@@ -15,7 +15,8 @@ object HelloWorld extends App {
     conf.setMaster("local[2]")
     val sc = new SparkContext(conf)
 
-    val lines = sc.textFile("data/spark/helloworld.txt")
+    val lines = sc.parallelize(Seq("hello world", "hello demo-tutorial"))
+//    val lines = sc.textFile("data/spark/helloworld.txt")
     val wc = lines.flatMap(_.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
     wc.foreach(println)
 
